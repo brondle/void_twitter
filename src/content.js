8f2afec2f2a1179d6b212117246d7e64258e0784
Brent Bailey <bb2906@nyu.edu>
2020-11-29 22:33:02 -0500

    first day's work. the website screams

--- src/content.js
+++ src/content.js
@@ -1,1 +1,31 @@
-console.log('Hello Content');
+console.log(document.readyState)
+let baseMode = true;
+chrome.storage.sync.get('data', (result) => {
+console.log('foo')
+if ('data' in result) {
+  for (const [key, value] of Object.entries(result.data)) {
+    if (value == true) {
+      baseMode = false;
+      break;
+    }
+  }
+}
+})
+const timer = ms => new Promise(res => setTimeout(res, ms))
+async function edit() {
+    let i = 1
+    while (i > 0) {
+      let tweets = document.getElementsByTagName('article')
+      let tweet_text = [...tweets].map(tweet => {
+        return [...tweet.getElementsByTagName('span')]
+      })
+      let flat = tweet_text.flat()
+      console.log('flat: ', flat)
+      for (var tweet of flat) {
+        //  console.log('tweet: ', tweet)
+        tweet.innerText = 'AHHHHHHHHHHHHHHHH'
+      }
+      await timer(i*1000);
+    }
+  }
+edit();
--- src/views/Options/App.js
+++ src/views/Options/App.js
@@ -1,25 +1,82 @@
-import React from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Options page</p>
-        <p>
-          Edit <code>src/views/Options/App.js</code> and save.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Option extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+      name: ''
+    };
+  }
+
+  render() {
+    return (
+      <div class="option">
+      <label>{this.props.name }</label>
+      <input
+        name={this.props.name}
+        type="checkbox"
+        checked={this.props.value}
+        onChange = {this.props.onChange}
+      />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        crabMode: false,
+        usefulMode: false,
+        silentMode: false,
+        username: ''
+      }
+    }
+  }
+
+componentWillMount() {
+  // On app load, check if there's existing data. If not, set it.
+  chrome.storage.sync.get('data', (result) => {
+    //use "in" check as a regular if(result.allData) will
+    //return false for empty arrays
+
+    console.log('result data', result.data)
+    if ( 'data' in result ) {
+      this.setState({ data: result.data });
+    } else chrome.storage.sync.set({ data: {} });
+  });
+  this.handleChange = this.handleChange.bind(this);
+}
+
+handleChange(event) {
+  const target = event.target;
+  const value = target.type === 'checkbox' ? target.checked : target.value;
+  const name = (target.name.charAt(0).toLowerCase() + target.name.slice(1)).replace(' ', '');
+  let data = {...this.state.data};
+  data[name] = value;
+  this.setState({
+    data
+  });
+  console.log('new data: ', data)
+  chrome.storage.sync.set({ data: data });
+}
+
+  render() {
+    return (
+      <div className='App'>
+      <form>
+        <input name="Username" type="text" value={this.state.username} onChange={this.handleChange}/>
+        <Option name="Crab Mode" value={this.state.crabMode} onChange={this.handleChange}/>
+        <Option name="Useful Mode" value={this.state.usefulMode} onChange={this.handleChange}/>
+          <Option name="Silent Mode" value={this.state.silentMode} onChange={this.handleChange}/>
+      </form>
+      </div>
+    );
+  }
 }
 
 export default App
